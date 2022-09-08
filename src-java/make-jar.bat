a6465aa81a6a0f646e9e955bdb993bd811518861
stephentetley <stephen.tetley@gmail.com>
2022-09-08 22:22:16 +0100

    Rebuilt java lib with Java 11 compiler. Updated the bat file so it runs java11 on my local setup.

--- src-java/make-jar.bat
+++ src-java/make-jar.bat
@@ -1,5 +1,20 @@
 
-set %JAVA_HOME%="C:\Program Files\Zulu\zulu-11"
+setlocal
+
+set JAVA_HOME=C:\Program Files\Zulu\zulu-11
+
+echo %JAVA_HOME%
+
+set PATH=C:\Program Files\Zulu\zulu-11\bin;%PATH%
+
+java --version
 
 ant
 
+rem check major version - should be 55...
+
+javap -cp .\build\interop-base-java.*.jar -verbose flixinterop.nio.file.Pathz
+
+
+
+endlocal
